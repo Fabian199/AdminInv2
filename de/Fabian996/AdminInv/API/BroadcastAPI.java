78f9505f0e90fb61aee53cf979d1085975e53783
Fabian199 <blauerdrache2@aol.com>
2016-01-22 20:01:47 +0100

    Create BroadcastAPI.java

---
+++ de/Fabian996/AdminInv/API/BroadcastAPI.java
@@ -0,0 +1,13 @@
+package de.Fabian996.AdminInv.API;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BroadcastAPI extends JavaPlugin{
+	
+	public static void broadcastMsg(String msg) {
+		msg = ChatColor.translateAlternateColorCodes('&', msg);
+		Bukkit.getServer().broadcastMessage(msg);
+	  }
+}
