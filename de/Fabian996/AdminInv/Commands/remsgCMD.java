8ef087915a2b30f4e37bc256dadc6b75f46ca043
Fabian199 <blauerdrache2@aol.com>
2016-01-31 07:21:03 +0100

    Create remsgCMD.java

---
+++ de/Fabian996/AdminInv/Commands/remsgCMD.java
@@ -0,0 +1,42 @@
+package de.Fabian996.AdminInv.Commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class remsgCMD implements CommandExecutor {
+	
+	public static final String Prefix = "§8[§4AdminInv§8]§r ";
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
+		if(cmd.getName().equalsIgnoreCase("r")){
+			Player p = (Player)cs;
+			if(args.length >= 0){
+				if(msgCMD.LAST_MSG_SEND_TO.containsKey(cs.getName())){
+					CommandSender target;
+					if(((String)msgCMD.LAST_MSG_SEND_TO.get(cs.getName())).equalsIgnoreCase("CONSOLE"))
+						target = Bukkit.getConsoleSender();
+					else{
+						target = Bukkit.getPlayer((String)msgCMD.LAST_MSG_SEND_TO.get(cs.getName()));
+					}
+					if(target != null){
+						String message = "".replace("§", "&");
+						for (int i = 0; i < args.length; i++) {
+							message = message + args[i] + " ";
+						}
+						msgCMD.SendMsg(cs, target, message);
+					}else{
+						p.sendMessage(Prefix + "§cYou cannot correspond yourself.");
+					}
+				}else{
+					p.sendMessage(Prefix + "§cError: You have still written with nobody ");
+				}
+			}
+		}
+		return true;
+	}
+}
