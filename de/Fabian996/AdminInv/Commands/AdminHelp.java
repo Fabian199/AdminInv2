354ccd95caca5620a11d52700e58b43c0c1a0081
Fabian199 <blauerdrache2@aol.com>
2016-01-31 07:17:19 +0100

    Update AdminHelp.java

--- de/Fabian996/AdminInv/Commands/AdminHelp.java
+++ de/Fabian996/AdminInv/Commands/AdminHelp.java
@@ -19,14 +19,13 @@ public class AdminHelp implements CommandExecutor {
 		if(!(cs instanceof Player)){
 			cs.sendMessage("You must be a player!");
 		}
-		if(cmd.getName().equalsIgnoreCase("adminhelp")){
+		if(cmd.getName().equalsIgnoreCase("ahelp")){
 			Player p = (Player)cs;
 			if(p.hasPermission("AdminInv.Help") || p.isOp()){
-				p.sendMessage(Prefix + "§n§fAdminInv §f made by §4Fabian996");
-				p.sendMessage(Prefix + "§8----------------- §7Commands §8-----------------");
+				p.sendMessage(Prefix + "§8----------------- §4Commands §8-----------------");
 				p.sendMessage(Prefix + "§6/admin §f- Open the Admin GUI  ");
 				p.sendMessage(Prefix + "§6/ghast §f- You get the Admin Item in the inventory ");
-				p.sendMessage(Prefix + "§6/warp §f - You are teleported to the Warp");
+				p.sendMessage(Prefix + "§6/warp §5[warpname]§f - You are teleported to the Warp");
 				p.sendMessage(Prefix + "§6/afk §f - You change to afk");
 				p.sendMessage(Prefix + "§6/back §f - You comeback form afk");
 				p.sendMessage(Prefix + "§6/ping §f - You show your Ping§7(ms)");
@@ -37,8 +36,11 @@ public class AdminHelp implements CommandExecutor {
 				p.sendMessage(Prefix + "§6/sinfo §f - Show Server Information");
 				p.sendMessage(Prefix + "§6/tc §5<message> §f - You send message in the TeamChat");
 				p.sendMessage(Prefix + "§6/cc §f - Clear the Global Chat");
+				p.sendMessage(Prefix + "§6/msg §7<player> §5<message> §f - Private Message");
+				p.sendMessage(Prefix + "§6/r §5<message> §f - Private Message ");
+				p.sendMessage(Prefix + "§6/tp §7<player> §f - You can teleport to player ");
 			}
 		}
-	return true;
+		return true;
 	}
 }
