166932f0690f244d2263d0b84853f1db6a57be5f
Fabian199 <blauerdrache2@aol.com>
2016-02-06 11:37:31 +0100

    Update TeamChatCMD.java

--- de/Fabian996/AdminInv/Commands/TeamChatCMD.java
+++ de/Fabian996/AdminInv/Commands/TeamChatCMD.java
@@ -13,7 +13,7 @@ public class TeamChatCMD implements CommandExecutor{
 	public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
 		Player p = (Player)cs;
 		if(cmd.getName().equalsIgnoreCase("tc")){
-			if(p.hasPermission("AdminInv.TeamChat")){
+			if(p.hasPermission("AdminInv.TeamChat") || p.hasPermission("AdminInv.*")){
 			      if (args.length != 0) {
 			          String message = "";
 			          String Prefix = "§8<§4TeamChat§8>§r ";
@@ -21,7 +21,7 @@ public class TeamChatCMD implements CommandExecutor{
 			            message = message + " " + args[i];
 			          }
 			          for(Player target : Bukkit.getOnlinePlayers()){
-			        	  if(target.hasPermission("AdminInv.TeamChat")){
+			        	  if(target.hasPermission("AdminInv.TeamChat") || p.hasPermission("AdminInv.*")){
 			        		  message = ChatColor.translateAlternateColorCodes('&', message);
 			        		  target.sendMessage(Prefix + "§c" + p.getDisplayName() + " §6>>§f "  + message);
 			        	  }
