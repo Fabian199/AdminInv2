c7264bc31a662fb1f329784534adb0d3d5658b70
Fabian199 <blauerdrache2@aol.com>
2016-02-06 11:27:55 +0100

    Update AdminInventory.java

--- de/Fabian996/AdminInv/GUI/AdminInventory.java
+++ de/Fabian996/AdminInv/GUI/AdminInventory.java
@@ -18,18 +18,18 @@ public class AdminInventory implements CommandExecutor {
 
 	@SuppressWarnings("unused")
 	private static AdminMain instance;
-	
+
 	public Inventory Inv = null;
 	
 	@Override
 	public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
 		Player pl = (Player)cs;
 		if(cmd.getName().equalsIgnoreCase("admin")){
-			if(pl.hasPermission("AdminInv.Admin")){
+			if(pl.hasPermission("AdminInv.Admin") || pl.hasPermission("AdminInv.*")){
 				pl.playSound(pl.getLocation(), Sound.FIREWORK_BLAST, 100.0F, 16.0F);
 				Inv = pl.getPlayer().getServer().createInventory(null, 27, "§0Admin Inventory");
 				// Inventar Erstellt...
-				
+					
 				// Icons im Inventar festlegen..
 				ItemStack Heal = new ItemStack(Material.POTION);
 				ItemMeta Healmeta = Heal.getItemMeta();
@@ -50,7 +50,7 @@ public class AdminInventory implements CommandExecutor {
 				ItemStack Weather = new ItemStack(Material.WATER_BUCKET);
 				ItemMeta Weathermeta = Weather.getItemMeta();
 				ArrayList<String> WeatherM = new ArrayList<String>();
-				Weathermeta.setDisplayName("§2Weather Inventory");
+				Weathermeta.setDisplayName("§2Weather/Time Inventory");
 				WeatherM.add("Here you see all Weather function");
 				Weathermeta.setLore(WeatherM);
 				Weather.setItemMeta(Weathermeta);
@@ -71,10 +71,23 @@ public class AdminInventory implements CommandExecutor {
 				Diffimeta.setLore(Diff);
 				Diffi.setItemMeta(Diffimeta);
 				
+				ItemStack Extra = new ItemStack(Material.SNOW_BLOCK);
+				ItemMeta Extrameta = Extra.getItemMeta();
+				ArrayList<String> ExtraI = new ArrayList<String>();
+				Extrameta.setDisplayName("§5Extra Inventory");
+				ExtraI.add("§fHere you see all Extra function");
+				Extrameta.setLore(ExtraI);
+				Extra.setItemMeta(Extrameta);
+				
 				ItemStack Reload = new ItemStack(Material.SPONGE);
 				ItemMeta Reloadmeta = Reload.getItemMeta();
 				Reloadmeta.setDisplayName("§5Reload the Server");
 				Reload.setItemMeta(Reloadmeta);
+				
+				ItemStack ConfigReload = new ItemStack(Material.BOOK_AND_QUILL);
+				ItemMeta ConfigReloadmeta = ConfigReload.getItemMeta();
+				ConfigReloadmeta.setDisplayName("§2Config Reload");
+				ConfigReload.setItemMeta(ConfigReloadmeta);
 				
 				ItemStack Stop = new ItemStack(Material.BARRIER);
 				ItemMeta Stopmeta = Stop.getItemMeta();
@@ -87,13 +100,14 @@ public class AdminInventory implements CommandExecutor {
 				Inv.setItem(5, Diffi);
 				Inv.setItem(8, Fly);
 				
+				Inv.setItem(18, ConfigReload);						
 				Inv.setItem(21 , Reload);
+				Inv.setItem(22, Extra);
 				Inv.setItem(23 , Stop);
-				
+						
 				pl.getPlayer().openInventory(Inv);
-
 			}
-		}
+		}		
 		return false;
 	}
 }
