81866464215aaadca0b22d22584d4b9f1cd3b837
Fabian199 <blauerdrache2@aol.com>
2016-02-06 11:25:46 +0100

    Update AdminFunction.java

--- de/Fabian996/AdminInv/Function/AdminFunction.java
+++ de/Fabian996/AdminInv/Function/AdminFunction.java
@@ -1,17 +1,25 @@
 package de.Fabian996.AdminInv.Function;
 
+import java.io.File;
 import java.util.ArrayList;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
+
 public class AdminFunction implements Listener {
 	
 	public static final String Prefix = "§8[§4AdminInv§8]§r ";
+	private File file = new File("plugins/AdminInv", "config.yml");
+	private FileConfiguration cfg = YamlConfiguration.loadConfiguration(this.file);
 	
+
 	@SuppressWarnings("unused")
 	private ArrayList<Player> vanished = new ArrayList<>();
 
@@ -25,6 +33,7 @@ public class AdminFunction implements Listener {
 			if(e.getCurrentItem().getType() == Material.POTION){
 				p.setHealth(20);
 				p.setFoodLevel(20);
+				p.setNoDamageTicks(Integer.valueOf(10));
 				p.sendMessage(Prefix + "§3You were cured");
 				e.getView().close();
 			}
@@ -57,11 +66,31 @@ public class AdminFunction implements Listener {
 				e.getView().close();
 				p.performCommand("diffis");
 			}
+			
+			if(e.getCurrentItem().getType() == Material.SNOW_BLOCK){
+					if(cfg.getBoolean("Inventory.ExtraInv", true)){
+						e.getView().close();
+						p.performCommand("extra");
+					}else if(cfg.getBoolean("Inventory.ExtraInv", false)){
+						e.getView().close();
+						p.sendMessage(Prefix + "You can Enable in the Config");
+					}		
+			}
 
 			if(e.getCurrentItem().getType() == Material.SPONGE){
 				e.getView().close();
 				p.performCommand("rl");
-				p.sendMessage((Prefix + "§7%p%§2 §6has been Server Reload").replace("%p%",p.getName()));
+    			for( Player target : Bukkit.getOnlinePlayers()){
+    				for(int i = 0; i < 120; i++){
+    					target.sendMessage(" ");
+    				}
+    				target.sendMessage(Prefix + "§7Chat has been cleared");
+    				}
+			}
+			
+			if(e.getCurrentItem().getType() == Material.BOOK_AND_QUILL){
+				e.getView().close();
+				p.performCommand("areload");
 			}
 			
 			if(e.getCurrentItem().getType() == Material.BARRIER){
