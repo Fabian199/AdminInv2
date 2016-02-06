3de2a8fe5a774ba8fd4d931d8acd375791e6bfcf
Fabian199 <blauerdrache2@aol.com>
2016-02-06 11:32:57 +0100

    Update AdminHelp.java

--- de/Fabian996/AdminInv/Commands/AdminHelp.java
+++ de/Fabian996/AdminInv/Commands/AdminHelp.java
@@ -21,25 +21,60 @@ public class AdminHelp implements CommandExecutor {
 		}
 		if(cmd.getName().equalsIgnoreCase("ahelp")){
 			Player p = (Player)cs;
-			if(p.hasPermission("AdminInv.Help") || p.isOp()){
-				p.sendMessage(Prefix + "§8----------------- §4Commands §8-----------------");
-				p.sendMessage(Prefix + "§6/admin §f- Open the Admin GUI  ");
-				p.sendMessage(Prefix + "§6/ghast §f- You get the Admin Item in the inventory ");
-				p.sendMessage(Prefix + "§6/warp §5[warpname]§f - You are teleported to the Warp");
-				p.sendMessage(Prefix + "§6/afk §f - You change to afk");
-				p.sendMessage(Prefix + "§6/back §f - You comeback form afk");
-				p.sendMessage(Prefix + "§6/ping §f - You show your Ping§7(ms)");
-				p.sendMessage(Prefix + "§6/pinfo §7<player> §f - Show PlayerInfo form one Player");
-				p.sendMessage(Prefix + "§6/kick §7<player> §5[reason] §f - Show Player Info");
-				p.sendMessage(Prefix + "§6/br §5<message> §f - You can Broadcast Message send");
-				p.sendMessage(Prefix + "§6/vote §f - Show all Votelinks");
-				p.sendMessage(Prefix + "§6/sinfo §f - Show Server Information");
-				p.sendMessage(Prefix + "§6/tc §5<message> §f - You send message in the TeamChat");
-				p.sendMessage(Prefix + "§6/cc §f - Clear the Global Chat");
-				p.sendMessage(Prefix + "§6/msg §7<player> §5<message> §f - Private Message");
-				p.sendMessage(Prefix + "§6/r §5<message> §f - Private Message ");
-				p.sendMessage(Prefix + "§6/tp §7<player> §f - You can teleport to player ");
-			}
+			if(p.hasPermission("AdminInv.Help") || p.isOp() || p.hasPermission("AdminInv.*")){
+				 if(args.length == 0){
+	                    p.sendMessage(Prefix + "§8--------------- §4Commands §7[§31/3§7]§8 ---------------");
+	                    p.sendMessage(Prefix + "§6/admin §f- Open the Admin GUI");
+	                    p.sendMessage(Prefix + "§6/ghast §f- You get the Admin Item in the Inventory");
+	                    p.sendMessage(Prefix + "§6/warp §5[warpname] §f- You are teleported to the Warp");
+	                    p.sendMessage(Prefix + "§6/afk §f- You Change to AFK");
+	                    p.sendMessage(Prefix + "§6/back §f-  You comeback from AFK");
+	                    p.sendMessage(Prefix + "§6/ping §f-  You Show your Ping§7(ms)");
+	                    p.sendMessage(Prefix + "§6/pinfo §7<Player> §f- Show Playerinfo from one Player");
+	                    p.sendMessage(Prefix + " ");
+	                    p.sendMessage(Prefix + "§fUse §6/ahelp 2 for next Page");
+	                    p.sendMessage(Prefix + "§8--------------- §4Commands §7[§31/3§7]§8 ---------------");
+	                    return true;
+	                }
+				 if(args.length == 1){
+					 if(args[0].equalsIgnoreCase("2")){
+						 p.sendMessage(Prefix + "§8--------------- §4Commands §7[§32/3§7]§8 ---------------");
+						 p.sendMessage(Prefix + "§6/kick §7<Player> §5[reason] §f- Kick a Player with Reason");
+						 p.sendMessage(Prefix + "§6/br §5<message> §f- You can send Broadcast Messages");
+						 p.sendMessage(Prefix + "§6/vote §f- Show all Vote Links");
+						 p.sendMessage(Prefix + "§6/sinfo §f- Show Server Information");
+						 p.sendMessage(Prefix + "§6/tc §5<message> §- Send a Message in the TeamChat");
+						 p.sendMessage(Prefix + "§6/cc §f- Clear the GlobalChat)");
+						 p.sendMessage(Prefix + "§6/msg §7<player> §5<messafe> §7- Private Message");
+						 p.sendMessage(Prefix + " ");
+						 p.sendMessage(Prefix + "§fUse §6/ahelp 3 for next Page");
+						 p.sendMessage(Prefix + "§8--------------- §4Commands §7[§32/3§7]§8 ---------------");
+						 return true;
+					 }  
+					 if(args[0].equalsIgnoreCase("3")){
+						 p.sendMessage(Prefix + "§8--------------- §4Commands §7[§33/3§7]§8 ---------------");
+						 p.sendMessage(Prefix + "§6/r §5<message> §f- Awnser on a Private Message ");
+						 p.sendMessage(Prefix + "§6/tp §7<player> §f- You can teleport to Player");
+						 p.sendMessage(Prefix + "§6/rename §f- You can rename Item");
+						 p.sendMessage(Prefix + "§6/gm §7<0/1/2/3/> §f- You can change to gamemode");
+						 p.sendMessage(Prefix + " ");
+						 p.sendMessage(Prefix + " ");
+						 p.sendMessage(Prefix + " ");
+						 p.sendMessage(Prefix + " ");
+						 p.sendMessage(Prefix + "§8--------------- §4Commands §7[§33/3§7]§8 ---------------");
+						 return true;
+					 }
+					 if(args[0].equalsIgnoreCase("warp")){
+						 p.sendMessage(Prefix + "§8-------------- §4 Warp Commands§8 ---------------");
+						 p.sendMessage(Prefix + "§6/setwarp §7<warpname> §f- set the warp point ");
+						 p.sendMessage(Prefix + "§6/delwarp §7<warpname> §f- delete the warp point");
+						 p.sendMessage(Prefix + "§6/warp §7<warpname> §f- You can teleport to the Warp point");
+						 p.sendMessage(Prefix + "§6/warps §f- You show all warp Points");
+						 p.sendMessage(Prefix + " ");
+						 p.sendMessage(Prefix + "§8-------------- §4 Warp Commands§8 --------------");
+					 }
+				 }
+			}	
 		}
 		return true;
 	}
