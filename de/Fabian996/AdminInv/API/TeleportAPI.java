6c664207f3eada5d1edd289668a40f5f1d177003
Fabian199 <blauerdrache2@aol.com>
2016-06-05 10:15:58 +0200

    Update TeleportAPI.java

--- de/Fabian996/AdminInv/API/TeleportAPI.java
+++ de/Fabian996/AdminInv/API/TeleportAPI.java
@@ -1,11 +1,11 @@
-package de.Fabian996.AdminInv.API;
+package com.jimdo.Fabian996.AdminInv2.API;
 
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TeleportAPI extends JavaPlugin{
-	
+
 	public static void teleportPlayer(Player target, Location loc){
 		target.teleport(loc);
 	}
