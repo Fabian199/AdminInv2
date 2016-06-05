73b01926e95313316d576c96495da62708ac38ba
Fabian199 <blauerdrache2@aol.com>
2016-06-05 10:39:13 +0200

    Update VoteCMD.java

--- de/Fabian996/AdminInv/Commands/VoteCMD.java
+++ de/Fabian996/AdminInv/Commands/VoteCMD.java
@@ -1,37 +1,31 @@
-package de.Fabian996.AdminInv.Commands;
-
-import java.io.File;
+package com.jimdo.Fabian996.AdminInv2.Commands;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
-public class VoteCMD implements CommandExecutor {
-	
-	public static final String Prefix = "§8[§4AdminInv§8]§r ";
-	private File file = new File("plugins/AdminInv", "config.yml");
-	private FileConfiguration cfg = YamlConfiguration.loadConfiguration(this.file);
-	
+import com.jimdo.Fabian996.AdminInv2.Main.AdminInv;
+
+public class Vote implements CommandExecutor {
+
 	@Override
 	public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
 		Player p = (Player)cs;
 		if(cmd.getName().equalsIgnoreCase("vote")){
-			String Vote = this.cfg.getString("vote.message1");
-			String Vote2 = this.cfg.getString("vote.message2");
-			String Vote3 = this.cfg.getString("vote.message3");
-			String Vote4 = this.cfg.getString("vote.message4");
-			String Vote5 = this.cfg.getString("vote.message5");
+			String Vote = AdminInv.cfg.getString("vote.message1");
+			String Vote2 = AdminInv.cfg.getString("vote.message2");
+			String Vote3 = AdminInv.cfg.getString("vote.message3");
+			String Vote4 = AdminInv.cfg.getString("vote.message4");
+			String Vote5 = AdminInv.cfg.getString("vote.message5");
 			
-			p.sendMessage(Prefix + "§6+---------------+§4§nVote Links§r§6+----------------+");		
-			p.sendMessage(Prefix + "§7Vote 1: §b" + Vote);
-			p.sendMessage(Prefix + "§7Vote 2: §b" + Vote2);
-			p.sendMessage(Prefix + "§7Vote 3: §b" + Vote3);
-			p.sendMessage(Prefix + "§7Vote 4: §b" + Vote4);
-			p.sendMessage(Prefix + "§7Vote 5: §b" + Vote5);
-			p.sendMessage(Prefix + "§6+---------------+§4§nVote Links§r§6+----------------+");	
+			p.sendMessage(AdminInv.AdminPrefix + "§6+---------------+§bVote Links§6+----------------+");		
+			p.sendMessage(AdminInv.AdminPrefix + "§7Vote 1: §2" + Vote);
+			p.sendMessage(AdminInv.AdminPrefix + "§7Vote 2: §2" + Vote2);
+			p.sendMessage(AdminInv.AdminPrefix + "§7Vote 3: §2" + Vote3);
+			p.sendMessage(AdminInv.AdminPrefix + "§7Vote 4: §2" + Vote4);
+			p.sendMessage(AdminInv.AdminPrefix + "§7Vote 5: §2" + Vote5);
+			p.sendMessage(AdminInv.AdminPrefix + "§6+---------------+§bVote Links§6+----------------+");	
 			return true;
 		}
 		return true;
