5ea45e8577e209f9c0baae418b65858c83ec108a
Fabian199 <blauerdrache2@aol.com>
2016-06-05 10:23:47 +0200

    Update MySQL.java

--- de/Fabian996/AdminInv/Library/MySQL.java
+++ de/Fabian996/AdminInv/Library/MySQL.java
@@ -1,173 +1,84 @@
-package de.Fabian996.AdminInv.Library;
+package com.jimdo.Fabian996.AdminInv2.Library;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.logging.Logger;
 
-public class MySQL extends Database{
-	private String hostname = "localhost";
-	private String portnmbr = "3306";
-	private String username = "minecraft";
-	private String password = "";
-	private String database = "minecraft";
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
-	public MySQL(Logger log, String prefix, String hostname, String portnmbr, String database, String username, String password) {
-		super(log, prefix, "[MySQL] ");
-		this.hostname = hostname;
-		this.portnmbr = portnmbr;
-		this.database = database;
-		this.username = username;
-		this.password = password;
-	 }
+public class MySQL {
+	
+	private String host;
+	private int port;
+	private String user;
+	private String password;
+	private String database;
+	private Connection con;
+	private File f1 = new File("plugins/AdminInv2", "MySQL.yml");
+	private FileConfiguration config = YamlConfiguration.loadConfiguration(this.f1);
+	
+	public MySQL() throws Exception{
+		this.host = this.config.getString("AdminInv.MySQL.host");
+		this.port = this.config.getInt("AdminInv.MySQL.port");
+		this.user = this.config.getString("AdminInv.MySQL.user");
+	    this.password = this.config.getString("AdminInv.MySQL.password");
+	    this.database = this.config.getString("AdminInv.MySQL.database");
+	    openConnection();
+	}
 
-	protected boolean initialize(){
-		try{
-			Class.forName("com.mysql.jdbc.Driver");
-			return true;
-		} catch (ClassNotFoundException e) {
-			writeError("Class Not Found Exception: " + e.getMessage() + ".", true);
-		}return false;
+	public Connection openConnection() throws Exception{
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://" + this.host + ":" + this.port + "/" + this.database, this.user, this.password);
+		this.con = con;
+		return con;
 	}
 	
-	public Connection open(){
-		if (initialize()) {
-			String url = "";
-			try {
-				url = "jdbc:mysql://" + this.hostname + ":" + this.portnmbr + "/" + this.database;
-				return DriverManager.getConnection(url, this.username, this.password);
-			} catch (SQLException e) {
-				writeError(url, true);
-				writeError("Could not be resolved because of an SQL Exception: " + e.getMessage() + ".", true);
-			}
-		}
-		return null;
-	}
-
-	public void close(){
-		Connection connection = open();
-		try {
-			if (connection != null)
-				connection.close();
-		} catch (Exception e) {
-			writeError("Failed to close database connection: " + e.getMessage(), true);
-		}
-	}
-
 	public Connection getConnection(){
-		return open();
+		return this.con;
 	}
-
-	public boolean checkConnection(){
-		Connection connection = open();
-
-		return connection != null;
-	}
-
-	public ResultSet query(String query){
-		Connection connection = null;
-		Statement statement = null;
-		ResultSet result = null;
-		try {
-			connection = open();
-			statement = connection.createStatement();
-			result = statement.executeQuery("SELECT CURTIME()");
-
-			switch ($SWITCH_TABLE$lib$PatPeter$SQLibrary$DatabaseHandler$Statements()[getStatement(query).ordinal()]) {
-			case 1:
-				result = statement.executeQuery(query);
-				return result;
-			}
-
-			statement.executeUpdate(query);
-			return result;
-		}
-		catch (SQLException e) {
-			writeError("Error in SQL query: " + e.getMessage(), false);
-		}
-		return result;
-	}
-
-	private int[] $SWITCH_TABLE$lib$PatPeter$SQLibrary$DatabaseHandler$Statements() {
-		return null;
-	}
-
-	public PreparedStatement prepare(String query){
-		Connection connection = null;
-		PreparedStatement ps = null;
+	
+	public boolean hasConnection(){
 		try{
-			connection = open();
-			ps = connection.prepareStatement(query);
-			return ps;
-		} catch (SQLException e) {
-			if (!e.toString().contains("not return ResultSet"))
-				writeError("Error in SQL prepare() query: " + e.getMessage(), false);
-		}
-		return ps;
-	}
-
-	public boolean createTable(String query){
-		Statement statement = null;
-		try {
-			this.connection = open();
-			if ((query.equals("")) || (query == null)) {
-				writeError("SQL query empty: createTable(" + query + ")", true);
-				return false;
-			}
-
-			statement = this.connection.createStatement();
-			statement.execute(query);
-  	   return true;
-		} catch (SQLException e) {
-			writeError(e.getMessage(), true);
-			return false;
-  	 } catch (Exception e) {
-  		 writeError(e.getMessage(), true);
-  	 }return false;
-	}
-
-	public boolean checkTable(String table){
-		try{
-			Connection connection = open();
-
-			Statement statement = connection.createStatement();
-
-			ResultSet result = statement.executeQuery("SELECT * FROM " + table);
-
-    	 return result != null;
-		}catch (SQLException e){
-			if (e.getMessage().contains("exist")) {
-				return false;
-			}
-			writeError("Error in SQL query: " + e.getMessage(), false);
-
-			if (query("SELECT * FROM " + table) == null) return true; 
+			return(this.con != null) || (this.con.isValid(1));
+		}catch(SQLException e){
 		}
 		return false;
 	}
-
-	public boolean wipeTable(String table){
-		Statement statement = null;
-		String query = null;
-		try {
-			if (!checkTable(table)) {
-				writeError("Error wiping table: \"" + table + "\" does not exist.", true);
-				return false;
-			}
-
-			this.connection = open();
-			statement = this.connection.createStatement();
-			query = "DELETE FROM " + table + ";";
-			statement.executeUpdate(query);
-
-			return true;
-		} catch (SQLException e) {
-			if (!e.toString().contains("not return ResultSet"))
-				return false;
-			}
-		return false;
+	
+	public void closeRessources(ResultSet rs, PreparedStatement st){
+		if (rs != null) {
+			try{
+				rs.close();
+			}catch (SQLException localSQLException) {}
+	    }
+	    if (st != null) {
+	    	try{
+	    		rs.close();
+	    	}catch (SQLException localSQLException1) {}
+	    }
+	}
+	
+	public void closeConnetion(){
+		try{
+			this.con.close();
+	    }catch (SQLException ex){
+	    	ex.printStackTrace();
+	    }
+	    	this.con = null;
+	}
+	  
+	public void queryUpdate(String query){
+		Connection con = this.con;
+		PreparedStatement st = null;
+		try{
+			st = con.prepareStatement(query);
+			st.executeUpdate();
+		}catch (SQLException ex){
+			System.err.println("Failed to send update '" + query + "'!");
 		}
 	}
+}
