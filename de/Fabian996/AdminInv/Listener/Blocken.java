eb47e059d037ccc46b891aa92bcd3da329c89458
Fabian199 <blauerdrache2@aol.com>
2016-06-05 10:21:48 +0200

    Update Blocken.java

--- de/Fabian996/AdminInv/Listener/Blocken.java
+++ de/Fabian996/AdminInv/Listener/Blocken.java
@@ -1,4 +1,4 @@
-package de.Fabian996.AdminInv.Listener;
+package com.jimdo.Fabian996.AdminInv2.Listeners;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Sound;
@@ -9,37 +9,37 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.help.HelpTopic;
 
-public class Blocken implements Listener {
+import com.jimdo.Fabian996.AdminInv2.Main.AdminInv;
 
-	public static final String Prefix = "§8[§4AdminInv§8]§r ";
+public class Block implements Listener{
 	
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onUnknow(PlayerCommandPreprocessEvent e){
 		if(!(e.isCancelled())){
-			Player p = e.getPlayer();
+			Player p = (Player)e.getPlayer();
 			String msg = e.getMessage().split(" ") [0];
-			HelpTopic top = Bukkit.getServer().getHelpMap().getHelpTopic(msg);
-			if(top == null){
-				p.playSound(p.getLocation(), Sound.ITEM_BREAK, 2F, 1F);
-				p.sendMessage(Prefix + "§8[ §b§l! §8] §cWrong command ");
+			HelpTopic help = Bukkit.getServer().getHelpMap().getHelpTopic(msg);
+			if (help == null){
+				p.playSound(p.getLocation(), Sound.ITEM_SHIELD_BREAK, 2F, 1F);
+				p.sendMessage(AdminInv.AdminPrefix + "§8[ §b§l! §8] §cFalscher Befehl");
 				e.setCancelled(true);
 			}
 		}
 	}
 	
-	@EventHandler
-	public void onplugin(PlayerCommandPreprocessEvent e){
+	@EventHandler(priority = EventPriority.HIGH)
+	public void onPlugin(PlayerCommandPreprocessEvent e){
 		String args[] = e.getMessage().split(" ");
-		if((args[0].equalsIgnoreCase("/pl")) || (args[0].equalsIgnoreCase("/plugins")) || (args[0].equalsIgnoreCase("/?"))){
+		if((args[0].equalsIgnoreCase("/pl")) || (args[0].equalsIgnoreCase("/plugins")) || (args[0].equalsIgnoreCase("/?")) || (args[0].equalsIgnoreCase("/help"))){
 			Player p = e.getPlayer();
 			if(p.isOp()){
 				
 			}else{
 				e.setCancelled(true);
-				p.sendMessage(Prefix + " §cYou do not have permission");
+				p.playSound(p.getLocation(), Sound.BLOCK_PORTAL_TRAVEL, 1.5F, -1F);
+				p.sendMessage(AdminInv.AdminPrefix + AdminInv.NoPerm);
 			}
-		}
-	
+		}	
 	}
 
 }
