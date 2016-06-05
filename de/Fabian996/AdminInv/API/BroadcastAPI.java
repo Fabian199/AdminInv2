2249642d2bbf210508722a391adfb3510ba4f031
Fabian199 <blauerdrache2@aol.com>
2016-06-05 10:15:43 +0200

    Update BroadcastAPI.java

--- de/Fabian996/AdminInv/API/BroadcastAPI.java
+++ de/Fabian996/AdminInv/API/BroadcastAPI.java
@@ -1,11 +1,11 @@
-package de.Fabian996.AdminInv.API;
+package com.jimdo.Fabian996.AdminInv2.API;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BroadcastAPI extends JavaPlugin{
-	
+
 	public static void broadcastMsg(String msg) {
 		msg = ChatColor.translateAlternateColorCodes('&', msg);
 		Bukkit.getServer().broadcastMessage(msg);
