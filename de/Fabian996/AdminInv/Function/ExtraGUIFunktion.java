2146d5281bafebc7e7d73e38f1d9b8e5ead04154
Fabian199 <blauerdrache2@aol.com>
2016-06-16 11:28:37 +0200

    Update and rename ExtraFunction.java to ExtraGUIFunktion.java

--- de/Fabian996/AdminInv/Function/ExtraGUIFunktion.java
+++ de/Fabian996/AdminInv/Function/ExtraGUIFunktion.java
@@ -1,4 +1,4 @@
-package de.Fabian996.AdminInv.Function;
+package com.jimdo.Fabian996.AdminInv2.Funktion;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -7,14 +7,18 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
-public class ExtraFunction implements Listener {
+import com.jimdo.Fabian996.AdminInv2.Main.AdminInv;
+
+public class ExtraGUIFunktion implements Listener {
+
+	public ExtraGUIFunktion (AdminInv adminInv) {
 	
-	public static final String Prefix = "§8[§4AdminInv§8]§r ";
-	
+	}
+
 	@EventHandler
 	public void Inventory(InventoryClickEvent e){
 		Player p = (Player) e.getWhoClicked();
-		if(e.getInventory().getName().equalsIgnoreCase("§0Extra Inventory")){
+		if(e.getInventory().getName().equalsIgnoreCase("§5Extra Inventar")){
 			e.setCancelled(true);
 			
 			if(e.getCurrentItem().getType() == Material.BUCKET){
@@ -35,15 +39,15 @@ public class ExtraFunction implements Listener {
 			if(e.getCurrentItem().getType() == Material.ROTTEN_FLESH){
 				e.getView().close();
 				for(Player target : Bukkit.getServer().getOnlinePlayers()){
-					if(p.hasPermission("AdminInv.Ignore"));
-					target.kickPlayer("You was kick form Admin");	
+					if(!(p.hasPermission("admininv.ignore") || (!p.hasPermission("admininv.*"))));
+					target.kickPlayer("Du würdst vom der Console gekickt");	
 				}
 			}
 			
 			if(e.getCurrentItem().getType() == Material.SUGAR){
 				e.getView().close();
-				String message = "The Player §6" + p.getName() + " §rhas save the Worlds" ;
-				Bukkit.broadcastMessage(Prefix + message);
+				String message = "Der Spieler §6" + p.getName() + " §rHat die Welt gespeichert" ;
+				Bukkit.broadcastMessage(AdminInv.AdminPrefix + message);
 				Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "save-all");
 			}
 			
