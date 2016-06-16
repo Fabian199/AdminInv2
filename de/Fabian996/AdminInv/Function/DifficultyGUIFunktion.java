05f64c34277ce9320153d35cb86931c15a20cc25
Fabian199 <blauerdrache2@aol.com>
2016-06-16 11:28:17 +0200

    Update and rename DifficultyFunction.java to DifficultyGUIFunktion.java

--- de/Fabian996/AdminInv/Function/DifficultyGUIFunktion.java
+++ de/Fabian996/AdminInv/Function/DifficultyGUIFunktion.java
@@ -1,4 +1,4 @@
-package de.Fabian996.AdminInv.Function;
+package com.jimdo.Fabian996.AdminInv2.Funktion;
 
 import org.bukkit.Difficulty;
 import org.bukkit.Material;
@@ -7,35 +7,39 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
-public class DifficultyFunction implements Listener {
-	
-	public static final String Prefix = "§8[§4AdminInv§8]§r ";
-	
+import com.jimdo.Fabian996.AdminInv2.Main.AdminInv;
+
+public class DifficultyGUIFunktion implements Listener {
+
+	public DifficultyGUIFunktion(AdminInv adminInv) {
+	}
+
 	@EventHandler
 	public void Inventory(InventoryClickEvent e){
 		Player p = (Player) e.getWhoClicked();
-		if(e.getInventory().getName().equalsIgnoreCase("§0Difficulty Inventory")){
+		if(e.getInventory().getName().equalsIgnoreCase("§3Difficulty Inventar")){
 			e.setCancelled(true);
-					
+;
+			
 			//Difficult <Peaceful/Easy/Normal/Hard> 
 			if(e.getCurrentItem().getType() == Material.APPLE){
 				p.getWorld().setDifficulty(Difficulty.PEACEFUL);
-				p.sendMessage(Prefix + "§rDifficulty in world §e" + p.getLocation().getWorld().getName() + "§r set to §epeaceful");
+				p.sendMessage(AdminInv.AdminPrefix + "§eDifficulty in der Welt würde auf Peaceful gesetzt");
 				e.getView().close();
 			}
 			if(e.getCurrentItem().getType() == Material.ARROW){
 				p.getWorld().setDifficulty(Difficulty.EASY);
-				p.sendMessage(Prefix + "§rDifficulty in world §6" + p.getLocation().getWorld().getName() + "§r set to §6Easy");
+				p.sendMessage(AdminInv.AdminPrefix + "§aDifficulty in der Welt würde auf Easy gesetzt");
 				e.getView().close();
 			}
 			if(e.getCurrentItem().getType() == Material.ENCHANTMENT_TABLE){
 				p.getWorld().setDifficulty(Difficulty.NORMAL);
-				p.sendMessage(Prefix + "§rDifficulty in world §a" + p.getLocation().getWorld().getName() + "§r set to §aNormal");
+				p.sendMessage(AdminInv.AdminPrefix + "§7Difficulty in der Welt würde auf Normal gesetzt");
 				e.getView().close();
 			}
 			if(e.getCurrentItem().getType() == Material.TNT){
 				p.getWorld().setDifficulty(Difficulty.HARD);
-				p.sendMessage(Prefix + "§rDifficulty in world §4" + p.getLocation().getWorld().getName() + "§r set to §4Hard");
+				p.sendMessage(AdminInv.AdminPrefix + "§4Difficulty in der Welt würde auf Hard gesetzt");
 				e.getView().close();
 			}
 			
