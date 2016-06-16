ff2908bbe9f35742190634ef48f2282a1c9736c8
Fabian199 <blauerdrache2@aol.com>
2016-06-16 11:24:22 +0200

    Update and rename OnlinePlayerInv.java to OnlinePlayerGUI.java

--- de/Fabian996/AdminInv/GUI/OnlinePlayerGUI.java
+++ de/Fabian996/AdminInv/GUI/OnlinePlayerGUI.java
@@ -1,4 +1,6 @@
-package de.Fabian996.AdminInv.GUI;
+package com.jimdo.Fabian996.AdminInv2.GUI;
+
+import java.util.ArrayList;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -6,27 +8,23 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 
-import de.Fabian996.AdminInv.Main.AdminMain;
+import com.jimdo.Fabian996.AdminInv2.Main.AdminInv;
 
-public class OnlinePlayerInv implements CommandExecutor {
-
-	@SuppressWarnings("unused")
-	private static AdminMain instance;
-
-	public Inventory OnlinePlayer = null;
-
+public class OnlinePlayerGUI implements CommandExecutor {
+	
+	public OnlinePlayerGUI(AdminInv adminInv) {
+	}
 	
 	@Override
 	public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
 		Player p = (Player)cs;
 		if(cmd.getName().equalsIgnoreCase("onlinep")){
-			if(p.hasPermission("AdminInv.Admin") || p.hasPermission("AdminInv.*")){
-				OnlinePlayer = p.getPlayer().getServer().createInventory(null, 54, "§7>> §0Online Players §7<<");
+			if(p.hasPermission("admininv.admin") || p.hasPermission("admininv.*")){
+				AdminInv.OnleinPlayer = p.getPlayer().getServer().createInventory(null, 54, "§7>> §0Online Players §7<<");
 
 				ItemStack Glass = new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) 13);
 				ItemMeta Glassmeta = Glass.getItemMeta();
@@ -35,7 +33,7 @@ public class OnlinePlayerInv implements CommandExecutor {
 				
 				ItemStack Comming = new ItemStack(Material.NETHER_STAR);
 				ItemMeta Commingmeta = Comming.getItemMeta();
-				Commingmeta.setDisplayName("§5Comming Soon");
+				Commingmeta.setDisplayName("§5Kommt Bald");
 				Comming.setItemMeta(Commingmeta);
 				
 				ItemStack Right = new ItemStack(Material.SKULL_ITEM,1 ,(short) 3);
@@ -52,21 +50,30 @@ public class OnlinePlayerInv implements CommandExecutor {
 						Skullmeta.setOwner(onlineplayers.getDisplayName());
 						Skull.setItemMeta(Skullmeta);
 						
-						OnlinePlayer.addItem(Skull);
+						AdminInv.OnleinPlayer.addItem(Skull);
 					}
 				}
-				OnlinePlayer.setItem(45, Glass);
-				OnlinePlayer.setItem(46, Glass);
-				OnlinePlayer.setItem(47, Glass);
-				OnlinePlayer.setItem(48, Glass);
-				OnlinePlayer.setItem(49, Comming);
-				OnlinePlayer.setItem(50, Glass);
-				OnlinePlayer.setItem(51, Glass);
-				OnlinePlayer.setItem(52, Glass);
-				OnlinePlayer.setItem(53, Right);
+				
+				ItemStack Back = new ItemStack(Material.GOLD_NUGGET);
+				ItemMeta Backmeta = Back.getItemMeta();
+				ArrayList<String> BM = new ArrayList<String>();
+				Backmeta.setDisplayName("§aBack");
+				BM.add("Zurück zum Inventar");
+				Backmeta.setLore(BM);
+				Back.setItemMeta(Backmeta);
+				
+				AdminInv.OnleinPlayer.setItem(45, Back);
+				AdminInv.OnleinPlayer.setItem(46, Glass);
+				AdminInv.OnleinPlayer.setItem(47, Glass);
+				AdminInv.OnleinPlayer.setItem(48, Glass);
+				AdminInv.OnleinPlayer.setItem(49, Comming);
+				AdminInv.OnleinPlayer.setItem(50, Glass);
+				AdminInv.OnleinPlayer.setItem(51, Glass);
+				AdminInv.OnleinPlayer.setItem(52, Glass);
+				AdminInv.OnleinPlayer.setItem(53, Right);
 				
 						
-				p.getPlayer().openInventory(OnlinePlayer);
+				p.getPlayer().openInventory(AdminInv.OnleinPlayer);
 			}
 		}		
 		return true;
